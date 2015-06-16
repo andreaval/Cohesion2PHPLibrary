fc86bf1b7254b1391c34932c7923e61fe03974ef
Andrea Vallorani <andrea.vallorani@gmail.com>
2015-06-16 10:58:15 +0200

    first commit

---
+++ Cohesion2/test/login.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+require_once 'cohesion2/Cohesion2.class.php';
+try{
+    $cohesion=new Cohesion2;
+    $cohesion->setAuthRestriction('0,1');
+    //$cohesion->setCertificate('cohesion2.crt.pem','cohesion2.key.pem');
+    $cohesion->auth();
+}
+catch(Exception $e){
+    die($e->getMessage());
+}
+
+if($cohesion->isAuth()){
+    echo 'Utente autenticato: '.$cohesion->username.'<br>';
+    echo 'Id SSO: '.$cohesion->id_sso.'<br>';
+    echo 'Id Aspnet: '.$cohesion->id_aspnet.'<br>';
+    echo 'Profilo: '.var_export($cohesion->profile,1).'<br>';
+}
